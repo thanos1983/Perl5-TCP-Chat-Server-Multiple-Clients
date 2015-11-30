63714149aedeb871f851fc8725ba3ff514320f32
Athanasios Garyfalos <thanos.egw@gmail.com>
2015-12-01 00:05:29 +0100

    Updating checkArgument Input not complete yet

--- assignment5_Client.pl
+++ assignment5_Client.pl
@@ -10,6 +10,19 @@ sub checkArgumentInput {
 	die "\nUsage: [IP:PORT] [NickName]\n\n";
     }
 
+    my $column = ':';
+    my @numberOfOccurences = $_[0] =~ /$column/g;
+
+    if (@numberOfOccurences > 1) {
+	die "\nUsage: [IP:PORT] please do not use more than one column\n\n";
+    }
+
+    # we use 2 as a number in split, into two fields
+    my ($ip, $port) = split(/:/, $_[0], 2);
+
+    print "IP: $ip\n";
+    print "Port: $port\n";
+
     return;
 }
 
